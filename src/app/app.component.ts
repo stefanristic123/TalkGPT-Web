65118bbb43bbf8a54cbef01f696d18b38fdf8964
stefan.ristic <stefanristic123>
2023-03-18 19:02:17 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Configuration, OpenAIApi, ChatCompletionRequestMessage, ChatCompletionRequestMessageRoleEnum } from 'openai';
+import * as RecordRTC from 'recordrtc';
+import { DomSanitizer } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,144 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'talkgpt';
+ 
+  public transcribedText?: string;
+  response: any;
+  form = new FormGroup({
+    text: new FormControl(''),
+  });
+
+  api_key = "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";
+  configuration = new Configuration({apiKey: "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"});
+  openai = new OpenAIApi(this.configuration); 
+
+  record: any;
+  recording = false;
+  url: any;
+  error: any;
+  transcriptions: { transcript: any, role: any,audioUrl: any, response: any }[] = [];
+
+
+  constructor(private domSanitizer: DomSanitizer,private http: HttpClient) {  }
+
+
+  sanitize(url: string) {
+    return this.domSanitizer.bypassSecurityTrustUrl(url);
+  }
+
+  initiateRecording() {
+    this.recording = true;
+    let mediaConstraints = { video: false, audio: true};
+    navigator.mediaDevices.getUserMedia(mediaConstraints).then(this.successCallback.bind(this), this.errorCallback.bind(this));
+  }
+
+  successCallback(stream: any) {
+    var options: any = {
+      mimeType: "audio/wav",
+      numberOfAudioChannels: 1
+    };
+    var StereoAudioRecorder = RecordRTC.StereoAudioRecorder;
+    this.record = new StereoAudioRecorder(stream, options);
+    this.record.record();
+  }
+
+  stopRecording() {
+    this.recording = false;
+    this.record.stop(this.processRecording.bind(this));
+  }
+
+  public async processRecording(blob: any): Promise<any>  {
+    console.log(blob)
+    this.transcript(blob);
+  }
+
+  // public async transcript(audio: any): Promise<any> { 
+
+  //   this.url = URL.createObjectURL(audio);
+  //   console.log('akalala', this.openai)
+  //     const formData = new FormData();
+  //     formData.append('file', audio, 'test.wav');
+  //     formData.append('model', 'whisper-1');
+  //     // formData.append('language', 'en');
+  
+  //     const transcriptionResponse = await this.http.post<any>(
+  //       'https://api.openai.com/v1/audio/transcriptions',
+  //       formData,
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${this.api_key}`,
+  //           Accept: 'application/json',
+  //         },
+  //       }
+  //     ).toPromise();
+  
+  //     this.transcribedText = transcriptionResponse?.text || '';
+  //     console.log(this.transcribedText)
+
+  //     if(this.transcribedText){
+  //       this.onSubmit(this.transcribedText)
+  //       this.transcriptions.push({ transcript: this.transcribedText, audioUrl: audio });
+  //     }
+  // }
+
+  // public async onSubmit(data: any): Promise<any> { 
+  //   // let data: string = this.form.value.text!;
+  //   const completion = await this.openai.createChatCompletion({
+  //     model: 'gpt-3.5-turbo',
+  //     messages: [
+  //       { role: ChatCompletionRequestMessageRoleEnum.System, content: data },
+  //     ],
+  //   });
+
+  //   console.log(completion.data.choices[0].message);
+
+  //   this.response = completion.data.choices[0].message
+  //   return this.response;
+  // }
+
+
+  public async transcript(audio: any): Promise<any> { 
+    this.url = URL.createObjectURL(audio);
+    console.log('akalala', this.openai)
+      const formData = new FormData();
+      formData.append('file', audio, 'test.wav');
+      formData.append('model', 'whisper-1');
+  
+      const transcriptionResponse = await this.http.post<any>('https://api.openai.com/v1/audio/transcriptions', formData,
+        {
+          headers: {
+            Authorization: `Bearer ${this.api_key}`,
+            Accept: 'application/json',
+          },
+        }
+      ).toPromise();
+  
+      this.transcribedText = transcriptionResponse?.text || '';
+      console.log(this.transcribedText)
+
+      if(this.transcribedText){
+        const completion = await this.openai.createChatCompletion({
+          model: 'gpt-3.5-turbo',
+          messages: [
+            { role: ChatCompletionRequestMessageRoleEnum.System, content: this.transcribedText },
+          ],
+        });
+
+        console.log(completion.data.choices[0].message);
+
+        this.response = completion.data.choices[0].message
+
+        this.transcriptions.push({ 
+          transcript: this.transcribedText,
+          audioUrl: audio,
+          role: ChatCompletionRequestMessageRoleEnum.User,
+          response: this.response
+        });
+      }
+  }
+
+  errorCallback(error: any) {
+    this.error = 'Can not play audio in your browser';
+  }
+
 }
